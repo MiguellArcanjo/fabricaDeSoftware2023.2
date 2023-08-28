d12988e04ffa95186972c34b9c770300d8871db6
Miguel <autologon@CSED.LAB>
2023-08-28 18:02:55 -0300

    teste com meu nome

--- Condicionais/ex1.py
+++ Condicionais/ex1.py
@@ -1,7 +1,7 @@
 idade = int(input("Digite sua idade: "))
 
 if idade >= 18:
-    print("Pode obter a CNH")
+    print("Pode obter a CNH.")
 
 else:
-    print("Não pode obter a CNH")
+    print("Não pode obter a CNH.")
